eade9280cf83d8e42d1d91a2e88f1efc580e248c
rustc0h1e <12ustcohle@gmail.com>
2018-04-25 15:30:38 +0530

    add current user to docker gorup

--- master.sh
+++ master.sh
@@ -43,3 +43,9 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+# =============================================
+# ===  add current user to docker group
+# =============================================
+sudo usermod -a -G docker $USER
+
+
